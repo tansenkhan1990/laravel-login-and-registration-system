210bf79865bbac14c1c5cb11536a73dfbe09f565
tansenkhan1990 <tansenkhan1990@gmail.com>
2019-07-16 11:07:44 +0200

    adding route link

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class RegistrationController extends Controller
 {
-    //
+
+    public function index()
+    {
+        return view('registration');
+    }
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('registration');
-});
+
+
+Route::get('/', 'RegistrationController@index');
